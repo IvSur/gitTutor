40a9a2cfc7b9e0dc4ff95d789bfcb8cbe3cedaad
Iv <neogenick@gmail.com>
2019-12-20 16:51:02 +0800

    new ex. Why wrong??

--- python.py
+++ python.py
@@ -1,4 +1,20 @@
 '''
-Напишите программу, которая переворачивает любое слово.
+Программа получает на вход одну фразу и если в этой фразе есть описание половины чего-либо,
+умножает на два. Как в примере ниже.
 '''
-print(str[::-1])
+str = input()
+
+if str[:4] == "пол-":
+    print(str.lower().replace("пол-", "два "))
+if str[:4] == "пол ":
+    print(str.lower().replace("пол ", "два "))
+elif str[:3] == "пол":
+    print(str.lower().replace("пол", "два "))
+elif " пол-" in str:
+    print(str.lower().replace(" пол-", " два "))
+elif " пол " in str:
+    print(str.lower().replace(" пол ", " два "))
+elif " пол" in str:
+    print(str.lower().replace(" пол", " два "))
+else:
+    print(str)
